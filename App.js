05e022c2c85f8cbaef7cc701c908bbbc3b601d38
Ninja77030 <n77030@outlook.com>
2023-09-04 18:26:10 +0530

    Added Fullscreen feature for the image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Dimensions, TouchableOpacity } from 'react-native';
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 
@@ -12,23 +12,26 @@ export default function App() {
     })
   }
   useEffect(() => { LoadMeme() }, [])
+  const [isFullScreen, setIsFullScreen] = useState(false)
+  const { width, height } = Dimensions.get('window')
+
   const [memeTitle, setMemeTitle] = useState('')
   const [image, setImage] = useState('')
   const [author, setAuthor] = useState('')
   return (
-    <View style={styles.container}>
+    <View style={{
+    flex: 1,
+    backgroundColor: isFullScreen ? 'black' : 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }}>
       <Text style={{ fontSize: 45, marginTop: 100 }}>{memeTitle}</Text>
-      <Image style={{ justifyContent: 'center', flex: 1, width: '50%', height: '50%', marginBottom: 34 }} source={{ uri: image }} />
+      <TouchableOpacity onPress={() => setIsFullScreen(!isFullScreen)}>
+        <Image style={{ width: isFullScreen? width : 300, height: isFullScreen? height : 300, resizeMode: 'contain' }} source={{ uri: image }} />
+      </TouchableOpacity>
       <Text style={{ marginTop: 10, fontSize: 15, marginBottom: 15 }}>Made by {author}</Text>
       <Button onPress={LoadMeme} title="New Meme" />
       <Text style={{ fontSize: 10, marginTop: 30 }}>Note: When you press the button, it might take some time to load a meme, so please be patient.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
